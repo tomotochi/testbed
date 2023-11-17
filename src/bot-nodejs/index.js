52e58dffd23c796eb2ee7b5c2ca0902dae4a329b
tomotochi <nyandertherlensis@gmail.com>
2023-11-17 09:20:48 +0900

    replaced variables

--- src/bot-nodejs/index.js
+++ src/bot-nodejs/index.js
@@ -19,19 +19,19 @@ const disClient = new DisClient({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
 })
 
-const guildID = process.env.DISCORD_GUILD_ID
-const userID = process.env.DISCORD_USER_ID
+const guildId = process.env.DISCORD_GUILD_ID
+const userId = process.env.DISCORD_USER_ID
 
 const targetItemIds = process.env.TARGET_ITEM_IDS.split(',')
 
-// unbClient.getUserBalance(guildID, userID).then(user => console.log(user))
-// unbClient.editUserBalance(guildID, userID, { cash: 5 })
-// unbClient.getInventoryItems(guildID, userID, { sort: 'id', page: 1 }).then(ret => console.log(ret.items))
+// unbClient.getUserBalance(guildId, userId).then(user => console.log(user))
+// unbClient.editUserBalance(guildId, userId, { cash: 5 })
+// unbClient.getInventoryItems(guildId, userId, { sort: 'id', page: 1 }).then(ret => console.log(ret.items))
 
 disClient.once(Events.ClientReady, (client) => {
   console.log(`Logged in as ${client.user.tag}`)
   // console.log(`Guild: ${disClient.guild}`)
-  // const guild = disClient.guilds.get(guildID)
+  // const guild = disClient.guilds.get(guildId)
   // const members = guild.members.map(member => member.id);
 	// console.log(members)
 
@@ -46,11 +46,11 @@ async function main() {
   // {id, name, items:[{id,quantity}]}
   result = []
 
-  const guild = disClient.guilds.cache.get(guildID)
+  const guild = disClient.guilds.cache.get(guildId)
   // list({after, cache, limit})
   const members = await guild.members.list()
   for ([k, v] of members) {
-    if (v.guild.id != guildID) continue
+    if (v.guild.id != guildId) continue
 
     const allMembers = await v.guild.members.fetch()
     for ([k, v] of allMembers) {
@@ -61,14 +61,14 @@ async function main() {
       // unbClient.getApplicationPermission(v.guild.id)
       //   .then(permission =>  console.log(`permission: ${JSON.stringify(permission.json)}`))
       
-      ret = await unbClient.getInventoryItems(guildID, v.user.id)
+      ret = await unbClient.getInventoryItems(guildId, v.user.id)
       result.push({
         id: v.user.id,
         name: v.user.username,
         items: ret.items.filter(e => targetItemIds.includes(e.itemId)).map(e => ({id: e.itemId, quantity: e.quantity}))
       })
 
-      // unbClient.getUserBalance(guildID, v.user.id)
+      // unbClient.getUserBalance(guildId, v.user.id)
       //   .then(user => console.log(user))
     }
     break
